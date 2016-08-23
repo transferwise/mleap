efb0af438bc1aed976fa0c5a5aa9d99ee62a32d5
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-08-23 00:25:01 -0400

    Update dependencies and SBT.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,19 +1,12 @@
 import sbt._
 
-object Dependencies {
+case class Dependencies(version: String) {
   val sparkVersion = "2.0.0"
-  val akkaVersion = "2.4.1"
-  val bundleMlVersion = "0.2.0-SNAPSHOT"
+  val bundleMlVersion = version
 
-  lazy val baseDependencies = Seq("org.scalatest" %% "scalatest" % "3.0.0-M15" % "test")
+  lazy val baseDependencies = Seq("org.scalatest" %% "scalatest" % "3.0.0" % "test")
 
-  lazy val sparkDependencies = Seq(
-    ("org.apache.spark" %% "spark-core" % sparkVersion)
-      .exclude("org.mortbay.jetty", "servlet-api")
-      .exclude("commons-beanutils", "commons-beanutils-core")
-      .exclude("commons-collections", "commons-collections")
-      .exclude("commons-logging", "commons-logging")
-      .exclude("com.esotericsoftware.minlog", "minlog"),
+  lazy val sparkDependencies = Seq("org.apache.spark" %% "spark-core" % sparkVersion,
     "org.apache.spark" %% "spark-sql" % sparkVersion,
     "org.apache.spark" %% "spark-mllib" % sparkVersion,
     "org.apache.spark" %% "spark-catalyst" % sparkVersion).map(_ % "provided")
