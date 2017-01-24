f8179609180fe9a983be01854984ff9c477dae02
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-01-23 18:33:37 -0800

    Update scala ARM to release version.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
       "org.apache.spark" %% "spark-catalyst" % sparkVersion)
     val avroDep = "org.apache.avro" % "avro" % "1.8.1"
     val sprayJson = "io.spray" %% "spray-json" % "1.3.2"
-    val arm = "com.jsuereth" %% "scala-arm" % "2.0-RC1"
+    val arm = "com.jsuereth" %% "scala-arm" % "2.0"
     val config = "com.typesafe" % "config" % "1.3.0"
     val scalaReflect = ScalaVersionDependentModuleID.versioned("org.scala-lang" % "scala-reflect" % _)
     val sparkAvro = "com.databricks" %% "spark-avro" % "3.0.1"
